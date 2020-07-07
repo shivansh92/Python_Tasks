a7a3b6a651e227dc62e40586e9bffc6a6f53da46
shivansh92 <shivansh.mehta@gmail.com>
2020-07-06 22:32:44 -0700

    Python Task-1

--- task-1.py
+++ task-1.py
@@ -1,1 +1,45 @@
+# Q-1
+x,y,z = 1,1.1,'hello'
+
+# Q-2
+imgNum = 5 + 3j
+intNum = 20
+imgNum = intNum
+
+# Q-3
+# with 'result' variable
+a = 20
+b = 10
+result = a
+a = b
+b = result
+
+# without 'result' variable
+a = 20
+b = 10
+a = a + b
+b = a - b
+a = a - b
+
+# Q-4
+# python2.x
+userInput = raw_input('Enter user Value:')
+print "User Value is: ", userInput
+
+# python3.x
+userInput = input('Enter user Value:')
+print("User Value is: ", userInput)
+
+# Q-5
+num1 = raw_input('Enter first Value(between 1-10):')
+num2 = raw_input('Enter second Value(between 1-10):')
+z = num1 + num2
+z += 30
+
+# Q-6
+userInput = raw_input('Enter user Value:')
+print "The input value datatype is ", type(userInput)
+
+# Q-7
+# Yes. In Implicit type conversion, Python automatically converts one data type to another data type. This process doesn't need any user involvement.
 
